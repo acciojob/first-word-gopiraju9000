888650c212c944b9ea93fed10b7d825209cc8fb1
gopiraju9000 <144523885+gopiraju9000@users.noreply.github.com>
2024-12-08 20:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function firstWord(s) {
   // your code here
+
+	let res_str = "";
+
+	for(let t of s){
+		if(res_str.length > 0 &&  t == ' '){
+			return res_str;
+		}
+		if(t != ' ') res_str = res_str + t;
+	}
+
+	return s;
+
+	
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
